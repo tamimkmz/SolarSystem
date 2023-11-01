be81a22ab76d37015e36a4420ae55e01bd765622
tamimkmz <tamim.kmz@gmail.com>
2023-11-01 16:29:51 -0700

    this commit has the first basic solar sys

---
+++ src/test/java/Main.java
@@ -0,0 +1,11 @@
+import SolarSystem.solarSystemBasics;
+
+public class Main {
+    public static void main(String[] args) {
+        solarSystemBasics solarsys=new solarSystemBasics();
+        solarsys.Sun="1";
+        solarsys.Moon="2";
+        solarsys.Planets="2";
+        solarsys.Stars="10";
+    }
+}
---
+++ src/test/java/SolarSystem/SolarSystemBasics.java
@@ -0,0 +1,9 @@
+package SolarSystem;
+
+public class SolarSystemBasics {
+    String Sun;
+    String Moon;
+    String Stars;
+    String Planets;
+
+}
